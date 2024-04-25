b30d6005bd11c139282bddea4e77f678152c87f8
Mizeva <117147204+Mizeva@users.noreply.github.com>
2024-04-25 18:37:22 +0300

    Update KP_Z_func.m

--- 3rd_task/KP_Z_func.m
+++ 3rd_task/KP_Z_func.m
@@ -1,12 +1,12 @@
 function [F] = KP_Z_func(k,E)
-    %U0 Ë E ‚ ˝¬
+    %U0 –∏ E –≤ —ç–í
     
     global a;
     global b;
     global U0;
-    mu = (4 .* sqrt(455) ./ 21) .* 1e9 .* sqrt(E);
-    lam = (4 .* sqrt(455) ./ 21) .* 1e9 .* sqrt(E - U0);
+    mu = sqrt((2 .* 9.1 .* 1e-31 ./ ((1.05 .* 1e-34) .^ 2))) .* sqrt(E);
+    lam = sqrt((2 .* 9.1 .* 1e-31 ./ ((1.05 .* 1e-34) .^ 2))) .* sqrt(E - U0);
     
-    F = cos(k .* (a + b)) - (cos(mu .* a) .* cos(lam .* b)) + ((mu .^ 2 + lam .^ 2) / (2 * mu * lam)) .* sin(lam .* b) .* sin(mu .* a);
+    F = cos(k .* (a + b)) - ((cos(mu .* a) .* cos(lam .* b)) - ((mu .^ 2 + lam .^ 2) / (2 .* mu .* lam)) .* sin(lam .* b) .* sin(mu .* a));
 end
 
